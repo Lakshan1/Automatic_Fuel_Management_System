d0662f1fe22bab0bfa0267262bcb71a56cba0916
Shanthakumar Lakshan <70530770+Lakshan1@users.noreply.github.com>
2022-11-18 09:28:03 +0530

    Update settings.py

--- automatic_fuel_management_system/settings.py
+++ automatic_fuel_management_system/settings.py
@@ -14,7 +14,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ye01w6g95eu(7=z+7xs5pr^%j^(0yb&)f5-%(%8n5=@pzg!wcn'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
