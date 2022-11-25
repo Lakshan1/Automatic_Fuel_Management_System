311fb69a9c5ae819c7a8bbc6342953f05fe241f0
Shanthkumr Lakshan <lak4837@gmail.com>
2022-11-25 11:58:07 +0530

    vechicle api updated

--- api/serializers.py
+++ api/serializers.py
@@ -1,9 +1,16 @@
 from rest_framework.serializers import ModelSerializer
-from base.models import VechicleUsers,Stations
+from base.models import VechicleUsers,Stations,VechicleTypes
 from django.contrib.auth.models import User
+from rest_framework import serializers
 
+class VechicleTypesSerializer(ModelSerializer):
+    class Meta:
+        model = VechicleTypes
+        fields = '__all__'
 
 class VechicleUserSerializer(ModelSerializer):
+    vechicle_type = VechicleTypesSerializer()
+
     class Meta:
         model = VechicleUsers
         fields = '__all__'
